8d6e8d70a42594564d158d1f03675d8c492c77d3
3dsonneto <edson_neto1996@hotmail.com>
2020-01-24 18:01:12 -0300

    integrando com o firebase

---
+++ js/init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAC_rjA3k4gFsztfhZ9uqEnaCF7mH37hB4",
+  authDomain: "capturaemail-a5ac5.firebaseapp.com",
+  databaseURL: "https://capturaemail-a5ac5.firebaseio.com",
+  projectId: "capturaemail-a5ac5",
+  storageBucket: "capturaemail-a5ac5.appspot.com",
+  messagingSenderId: "638631003394",
+  appId: "1:638631003394:web:773032b86db17b2ff96e1b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
